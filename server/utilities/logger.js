bc6ef782603e830aebe7f5f81e36b829216e2c33
IT21375132 <gokulabisheak12@gmail.com>
2023-07-14 18:41:29 +0530

    Updated Vercel Configs

--- server/utilities/logger.js
+++ server/utilities/logger.js
@@ -5,7 +5,7 @@ const logger = pino({
         target: 'pino-pretty',
         options: {
             colorize: true,
-            ignore: 'pid,hostname',
+            ignore: 'pid,hostname,time',
         }
     }
 })
