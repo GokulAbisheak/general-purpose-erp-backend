3507aaedeaaf468fa4c179e06eb95bddc7163055
IT21375132 <gokulabisheak12@gmail.com>
2023-07-15 00:46:52 +0530

    Added Pino Pretty

--- server/utilities/logger.js
+++ server/utilities/logger.js
@@ -1,5 +1,13 @@
-import pino from 'pino';
+import pino, { transport } from "pino";
 
-const logger = pino();
+const logger = pino({
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+      ignore: 'pid,hostname,time'
+    },
+  },
+});
 
 export default logger;
